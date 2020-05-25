b6e35a96ca179893ead4522a13f733342dd47abc
LucaTomala <luca.tomala@gmail.com>
2020-05-25 08:57:11 +0200

    finished

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,11 +1,12 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     private static final String CURRENCY_PATTERN = "\\$\\s?\\d+(\\.\\d{1,2})?";
     private static final Pattern currenyPATTERN = Pattern.compile(CURRENCY_PATTERN);
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        return currenyPATTERN.matcher(usCurrencyString);
     }
 }
