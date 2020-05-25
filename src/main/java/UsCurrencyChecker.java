5f90c408b94b521a4f70e847ced5aeae244d8b1f
LucaTomala <luca.tomala@gmail.com>
2020-05-25 08:55:29 +0200

    added static

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -3,6 +3,8 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.regex.Matcher;
 
 public class UsCurrencyChecker {
+    private static final String CURRENCY_PATTERN = "\\$\\s?\\d+(\\.\\d{1,2})?";
+    private static final Pattern currenyPATTERN = Pattern.compile(CURRENCY_PATTERN);
     public static Matcher getMatcher(String usCurrencyString) {
         throw new NotImplementedException();
     }
